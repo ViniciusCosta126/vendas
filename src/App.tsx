e6ce2edb484e2511a0f7796fc9730bde7d6a0c37
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-06-23 10:44:26 -0300

    feat: insert redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Loguin from './modules/login';
+import store from './store';
 export default function App() {
   return (
-    <SafeAreaView>
-      <Loguin />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView>
+        <Loguin />
+      </SafeAreaView>
+    </Provider>
   );
 }
--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -1,20 +1,21 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { setUserAction } from '../../store/reducers/useReducer';
 import { connectionAPIPost } from '../functions/connection/connectionAPI';
 import { RequestLoguin } from '../types/requestLogin';
 import { RerturnLogin } from '../types/returnLogin';
-import { UserType } from '../types/userType';
 
 export const useRequest = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
-  const [user, setUser] = useState<UserType>();
+  const dispatch = useDispatch();
 
   const authRequest = async (body: RequestLoguin) => {
     setLoading(true);
     await connectionAPIPost<RerturnLogin>('http://192.168.0.107:8080/auth', body)
       .then((result) => {
-        setUser(result.user);
+        dispatch(setUserAction(result.user));
       })
       .catch(() => {
         setErrorMessage('Usuario ou senha invalidos');
@@ -25,7 +26,6 @@ export const useRequest = () => {
     loading,
     errorMessage,
     authRequest,
-    user,
     setErrorMessage,
   };
 };
